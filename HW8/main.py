ec53baf76d2c626d268b87dc342f47c694eabad5
Elderhun <kiselovigor970@gmail.comm>
2022-08-14 16:45:44 +0300

    Home Worke

--- HW8/main.py
+++ HW8/main.py
@@ -1,9 +1,8 @@
 from interface import main
-import pandas as pd
-# main()
 
-df = pd.DataFrame([[1,'Bob', 'Builder'],
-                  [2,'Sally', 'Baker'],
-                  [3,'Scott', 'Candle Stick Maker']], 
-columns=['id','name', 'occupation'])
+
+
+
+main()
+
 
